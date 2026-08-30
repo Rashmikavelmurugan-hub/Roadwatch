8fb5682895ebd40043a4e89471b507fcc0fc27a0
Rashmika Velmurugan <rashmikavelmurugan@gmail.com>
2026-08-30 16:27:22 +0530

    Add Spring Boot application

---
+++ src/main/java/com/roadwatch/roadwatch/RoadwatchApplication.java
@@ -0,0 +1,13 @@
+package com.roadwatch.roadwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadwatchApplication.class, args);
+	}
+
+}
